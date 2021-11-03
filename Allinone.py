c005383a2282fe565c61bc846659bed287e986a3
CODE WITH RAWAN <91734162+codewithrawan@users.noreply.github.com>
2021-11-03 22:12:16 +0530

    Create Allinone.py

---
+++ Allinone.py
@@ -0,0 +1,24 @@
+# FOLLOW CODEWITHRAWAN FOR MORE
+line = "this box is amazing and it also has knowledge"
+
+print(line.count("a")) # to count the number of text in string
+
+print(line.capitalize()) #to capitalize first latter of string
+
+print(line.endswith("ing")) #to check the ending of the string
+
+print (len(line)) #to check the length of string
+
+print (line.find("also")) #to find the world in string
+
+# to find double spce
+
+a = ("To kese he   aap log");
+
+print(a.find("  "))
+
+# To make a quick greeting 
+
+greet= input("Enter The Name : ");
+
+print("Good Afternoon ", greet);
