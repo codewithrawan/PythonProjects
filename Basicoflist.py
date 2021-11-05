56e4db5e9d8794e390cef0406b9f7d4188c7aa37
CODE WITH RAWAN <91734162+codewithrawan@users.noreply.github.com>
2021-11-06 00:02:54 +0530

    Create Basicoflist.py

---
+++ Basicoflist.py
@@ -0,0 +1,28 @@
+# FOLLOW CODEWITHRAWAN FOR MORE
+
+list= [4,8,45,87,7,5,56] #this is a type of list
+
+list.sort()# to arrange in sequence
+
+print(list)
+
+list.remove(7)#to remove 7 form list
+
+print(list)
+
+list.append(123)#to add 123 in end of the list
+
+print(list)
+
+list.insert(2,143)#to add 143 at 2 index 
+
+print(list)
+
+list.reverse()#to reverse the list 
+
+print(list)
+
+list.pop(2)#to delet item on index 2
+
+print(list)
+
